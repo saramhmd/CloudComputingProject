6ea1ebae6961bf6975bc32690564b51bcddf824a
al-khateb <saram7md02@gmail.com>
2023-04-27 17:31:58 +0300

    sara1

--- app/src/main/java/com/example/cloudcomputingproject/DoctorHome.java
+++ app/src/main/java/com/example/cloudcomputingproject/DoctorHome.java
@@ -1,14 +1,17 @@
 package com.example.cloudcomputingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class DoctorHome extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_home);
+
     }
 }
