ccc53b72df7ebcc8489ab468e574af6e05be5d6e
al-khateb <saram7md02@gmail.com>
2023-04-27 17:34:36 +0300

    sara1

--- app/src/main/java/com/example/cloudcomputingproject/DoctorHome.java
+++ app/src/main/java/com/example/cloudcomputingproject/DoctorHome.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class DoctorHome extends AppCompatActivity {
     private RecyclerView recyclerView;
-
+//jhkjhk
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
