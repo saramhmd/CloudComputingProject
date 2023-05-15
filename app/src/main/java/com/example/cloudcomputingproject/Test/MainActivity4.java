cf54375a55c0f39c5d066eab2954eff62565c1d6
pc <hebamattar2002@gmail.com>
2023-05-15 22:24:32 +0300

    heba

---
+++ app/src/main/java/com/example/cloudcomputingproject/Test/MainActivity4.java
@@ -0,0 +1,16 @@
+package com.example.cloudcomputingproject.Test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cloudcomputingproject.R;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
