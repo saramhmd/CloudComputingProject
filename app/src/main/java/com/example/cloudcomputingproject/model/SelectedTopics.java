fa01000e11875c83b3fd86b82c6c3e9f9a88e3ee
al-khateb <saram7md02@gmail.com>
2023-05-20 22:14:08 +0300

    new

--- app/src/main/java/com/example/cloudcomputingproject/model/SelectedTopics.java
+++ app/src/main/java/com/example/cloudcomputingproject/model/SelectedTopics.java
@@ -1,6 +1,7 @@
 package com.example.cloudcomputingproject.model;
 
 public class SelectedTopics {
+    public String id;
     private String advice;
     private String imageUri;
     private String topicName;
@@ -8,14 +9,25 @@ public class SelectedTopics {
     public SelectedTopics() {
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getId() {
+        return id;
+    }
 
     public SelectedTopics(String advice, String topicName, String imageUri ) {
         this.advice = advice;
         this.topicName = topicName;
         this.imageUri = imageUri;
     }
-
+    public SelectedTopics(String id,String advice, String topicName , String imageUri ) {
+        this.imageUri = imageUri;
+        this.id = id;
+        this.advice = advice;
+        this.topicName = topicName;
+    }
     public String getImageUri() {
         return imageUri;
     }
